de86752a206b1c28949813cdcfd704f5a26d8f8f
KlukaCross <Jktu332@yandex.ru>
2022-09-22 14:19:31 +0300

    теперь дескриптор для записи в файл передаётся через exec добавлена обработка системных ошибок

--- lab1/child.c
+++ lab1/child.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "response.h"
 #include <stdlib.h>  // for strtol
+#include <errno.h>
 #define BUF_SIZE 2048
 
 int is_upper(char ch) {
@@ -10,14 +11,19 @@ int is_upper(char ch) {
 }
 
 int main(int argc, char* argv[]) {
+    errno = 0;
     int d_in = strtol(argv[1], NULL, 10);
-    FILE* f = fopen(argv[2], "w");
+    int d_out = strtol(argv[2], NULL, 10);
     int d_err = strtol(argv[3], NULL, 10);
     int now_ch;
     char st[BUF_SIZE] = "";
     int st_len = 0;
     while (1) {
         read(d_in, &now_ch, sizeof(int));
+        if (errno) {
+            perror("read error");
+            return errno;
+        }
         st[st_len] = (now_ch == EOF)? '\n' : (char) now_ch;
         st[++st_len] = '\0';
 
@@ -28,14 +34,16 @@ int main(int argc, char* argv[]) {
         if (!is_upper(st[0]))
             strcpy(res, "invalid string");
         else {
-            fwrite(st, sizeof(char), st_len, f);
+            write(d_out, &st, sizeof(char)*st_len);
+            if (errno) {
+                perror("write error");
+                return errno;
+            }
             strcpy(res, "OK");
         }
         write(d_err, &res, sizeof(char) * RESPONSE_TEXT_SIZE);
-        if (st[st_len-1] == EOF) {
-            fclose(f);
+        if (now_ch == EOF)
             return 0;
-        }
         st[st_len=0] = '\0';
     }
 }
--- lab1/main.c
+++ lab1/main.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <fcntl.h>
+#include <errno.h>
 #include "response.h"
 
 #define FILENAME_SIZE 256
@@ -27,21 +29,31 @@ int handler(int pipe_out, int pipe_in, int child_id) {
 }
 
 int main() {
+    errno = 0;
     int fd1[2];
     int fd2[2];
     char filename[FILENAME_SIZE];
     scanf("%s", filename);
+
+    int file_d = open(filename, O_WRONLY | O_CREAT, S_IRWXU);
+    if (file_d < 0) {
+        perror("open error");
+        return -1;
+    }
+
     pipe(fd1);
     pipe(fd2);
     pid_t id = fork();
     if (id < 0) {
-        printf("fork error");
+        perror("fork error");
         return -1;
     } else if (id == 0) {
-        char child_in[DESCRIPTOR_SIZE], child_err[DESCRIPTOR_SIZE];
+        char child_in[DESCRIPTOR_SIZE], child_err[DESCRIPTOR_SIZE], child_out[DESCRIPTOR_SIZE];
         sprintf(child_in, "%d", fd1[0]);
+        sprintf(child_out, "%d", file_d);
         sprintf(child_err, "%d", fd2[1]);
-        execl("./child.out", "./child.out", child_in, filename, child_err, (char *) NULL);
+        execl("./child.out", "./child.out", child_in, child_out, child_err, (char *) NULL);
+        perror("execl error");
     }
     int res = handler(fd1[1], fd2[0], id);
     return res;
