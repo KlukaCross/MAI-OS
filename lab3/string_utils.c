bdfdac9b9a47cf29feb79d27c64748b1db4fc86f
KlukaCross <Jktu332@yandex.ru>
2022-10-06 21:34:48 +0300

    create string_utils

---
+++ lab3/string_utils.c
@@ -0,0 +1,9 @@
+#include "string_utils.h"
+
+bool is_separator(char c) {
+    return c == ' ' || c == '\n';
+}
+
+__int128 str_to_int128(char st[], int *index) {
+
+}
---
+++ lab3/string_utils.h
@@ -0,0 +1,10 @@
+#ifndef LAB3_STRING_UTILS_H
+#define LAB3_STRING_UTILS_H
+
+#include <stdbool.h>
+
+bool is_separator(char c);
+
+__int128 str_to_int128(char st[], int *index);
+
+#endif //LAB3_STRING_UTILS_H
