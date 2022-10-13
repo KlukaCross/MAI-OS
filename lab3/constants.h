39072e4d03bb3364141c5801bb09d601ee636298
KlukaCross <Jktu332@yandex.ru>
2022-10-13 11:08:11 +0300

    изменены константы

--- lab3/constants.h
+++ lab3/constants.h
@@ -1,8 +1,8 @@
 #ifndef LAB3_CONSTANTS_H
 #define LAB3_CONSTANTS_H
 
-#define DEFAULT_MAX_THREADS 8
-#define DEFAULT_MAX_MEMORY 256 // in bytes
-#define MAX_128_HEX_BYTES 37 // max length of string 128-bit HEX number
+#define DEFAULT_MAX_THREADS 4
+#define DEFAULT_MAX_MEMORY 512 // in bytes
+#define MAX_128_HEX_BYTES 32
 
 #endif //LAB3_CONSTANTS_H
